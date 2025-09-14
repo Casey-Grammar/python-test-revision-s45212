1e25900d14fdfa40979b9f7bf06ec49ec055bc06
s45212 <s45212@caseygrammar.vic.edu.au>
2025-09-14 23:03:03 +0000

    done task 4

--- Task4.py
+++ Task4.py
@@ -5,7 +5,12 @@
 
 def main():
     #Write your code here
-    
+    temp = input('Whats is the password Ali? ')
+    if temp == 'Open sesame!':
+     print('The cave door opens!')
+    else:
+     print('The cave door doesnt respond?')
+
 
 
     # End of your code here
