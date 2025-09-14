defc11708c8628102363e170f2e9c59c6d30b0b2
s45212 <s45212@caseygrammar.vic.edu.au>
2025-09-14 23:00:58 +0000

    done task 3

--- Task3.py
+++ Task3.py
@@ -3,7 +3,10 @@
 # Write a program that asks the user
 def main():
     #Write your code for input here
-    
+    Dog_years = int(input('Dog years: '))
+    Huan_age = Dog_years * 7 
+    print(f'Human age = {Huan_age}')
+
 
 
     # End of your code for input here
