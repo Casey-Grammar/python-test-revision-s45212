e9a6ebb067156ce06d0f8f6a7d70ae2f2dfafcbe
s45212 <s45212@caseygrammar.vic.edu.au>
2025-09-14 23:04:59 +0000

    done task 6

--- Task6.py
+++ Task6.py
@@ -5,7 +5,17 @@
 
 def main():
     #Write your code here
-    
+    italy_score = input('Italy: ')
+    Brazil_socre = input('Brazil: ')
+    if italy_score < Brazil_socre:
+      print ('Brazil won the match.')
+    elif Brazil_socre > italy_score:
+      print ('Italy won the match.')
+    elif Brazil_socre == italy_score:
+      print ('The match was a draw')
+    else:
+      print()
+
         
     
     # End of your code here
