07bc939351ab49abbbdbab339a527725356ba650
s45212 <s45212@caseygrammar.vic.edu.au>
2025-09-14 23:10:05 +0000

    complete both 6 and 5

--- Task6.py
+++ Task6.py
@@ -5,24 +5,15 @@
 
 def main():
     #Write your code here
-    italy_score = input('Italy: ')
-    Brazil_socre = input('Brazil: ')
-    if italy_score < Brazil_socre:
-      print ('Brazil won the match.')
-    elif Brazil_socre > italy_score:
-      print ('Italy won the match.')
-    elif Brazil_socre == italy_score:
-      print ('The match was a draw')
-    else:
-      print()
+  word = str(input('Enter line: '))
+  if 'bear' in word:
+   print("There's a bear in there.")
+  else:
+   print('No bears here.')
+
 
         
     
     # End of your code here
-
-
-
-
-
 if __name__ == '__main__':
-    main()
+ main()
